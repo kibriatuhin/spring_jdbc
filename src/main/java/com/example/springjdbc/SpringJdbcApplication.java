5854adfa1490a8b84798ca0d36dac95eaac44ebc
kibriatuhin <tuinn78@gmail.com>
2022-10-02 01:49:59 +0600

    java based configuration

--- src/main/java/com/example/springjdbc/SpringJdbcApplication.java
+++ src/main/java/com/example/springjdbc/SpringJdbcApplication.java
@@ -1,10 +1,12 @@
 package com.example.springjdbc;
 
+import com.example.springjdbc.config.AppConfig;
 import com.example.springjdbc.dao.StudentDaoImpl;
 import com.example.springjdbc.entities.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -14,14 +16,15 @@ import java.util.List;
 public class SpringJdbcApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         System.out.println(context.getBeanDefinitionCount());
 
-        StudentDaoImpl studentDao = context.getBean("StudentDaoImpl",StudentDaoImpl.class);
+        StudentDaoImpl studentDao = context.getBean("studentImpl",StudentDaoImpl.class);
 
 
         List<Student> list = studentDao.getAllStudent();
         list.forEach(System.out::println);
+
 
     }
 
