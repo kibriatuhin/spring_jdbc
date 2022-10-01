bf6003a11f0efe87b3f53e34d82216c9ce9af980
kibriatuhin <tuinn78@gmail.com>
2022-10-01 22:40:51 +0600

    project upload successfully

---
+++ src/main/java/com/example/springjdbc/SpringJdbcApplication.java
@@ -0,0 +1,15 @@
+package com.example.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class SpringJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApplication.class, args);
+        
+    }
+
+}
